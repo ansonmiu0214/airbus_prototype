d8b1342d3bf1bee00dc483dfbd80a95c13975817
Anson Miu <ansonmiu0214@gmail.com>
2019-03-02 14:44:58 +0000

    Refactored buckled class for consistent output

--- public/index.js
+++ public/index.js
@@ -1,17 +1,19 @@
 const socket = io()
 
 const seatId = '6A'
+const buckledClass = 'buckled'
 const seat = document.getElementById(seatId).parentElement
 
 socket.on('buckle', () => {
   console.log('buckle event received')
 
-  seat.classList.toggle('buckled')
+  if (!seat.classList.contains(buckledClass)) seat.classList.add(buckledClass)
 
 })
 
 socket.on('unbuckle', () => {
   console.log('buckle event received')
 
-  seat.classList.toggle('buckled')
+  if (seat.classList.contains(buckledClass))
+    seat.classList.remove(buckledClass)
 })
